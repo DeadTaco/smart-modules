603860700284025e9170a8dd2fb6d86bc533bc17
devcrowley <dcrowley@woodrodgers.com>
2019-12-20 12:49:38 -0800

    Add menu items post-creation of menu object

--- src/modules/sm.menu.js
+++ src/modules/sm.menu.js
@@ -5,6 +5,63 @@ SmartModule.addModule("menu", (
         requires : [],
         version : "1.0",
         createMenu(menuElements, container) {
+            
+            // MenuNode contains all of the elements for a menu group and its child nodes (sub menus)
+            let MenuNode = function() {
+                this.name = "root_node";
+                this.subMenus = [];
+                this.isRoot = false;
+
+                // Retrieves a node based on its path, separated by dotted values, i.e. file.saveas.text finds the node in the path file -> saveas -> text
+                this.getNodeByPath = function(nodePath) {
+                    nodePath = nodePath.toLowerCase();
+                    let nodes = nodePath.split(".");
+                    let thisNode = this;
+                    let foundNode = false;
+                    for(let i = 0; i < nodes.length; i++) {
+                        foundNode = thisNode.getNodeByName(nodes[i]);
+                        if(foundNode) {
+                            thisNode = foundNode;
+                        }
+                    }
+                    return foundNode;
+                }
+                this.getNodeByName = function(name) {
+                    name = name.toLowerCase();
+                    let nodes = this.subMenus;
+                    for(let i = 0; i < nodes.length; i++) {
+                        if(this.subMenus[i].name.toLowerCase() == name)  return this.subMenus[i];
+                    }
+                    return false;
+                }
+
+                // Add a sub-menu item to an existing item
+                this.addItem = function(itemData) {
+                    let parentNode = this; 
+                    let parentDom = this.domElement;
+                    let isRoot = this.isRoot;
+                    if(isRoot) {
+                        // itemData.subMenus = [];
+                        this.domElement = this.domElement.parentElement;
+                    }
+                    // buildMenu([itemData], false, parentDom, parentNode);
+                    let menuGroupDiv = null;
+                    if(!this.domElement.querySelector(".sm-menu-group")) {
+                        menuGroupDiv = document.createElement("div");
+                        menuGroupDiv.classList.add("sm-menu-group");
+                        this.domElement.append(menuGroupDiv);
+                        if(!this.domElement.classList.contains("sm-menu-rootitem")) this.domElement.classList.add("sm-menu-haschildren");
+                    } else {
+                        menuGroupDiv = this.domElement.querySelector(".sm-menu-group");
+                    }
+                    let newMenuNode = addMenuNode(itemData, menuGroupDiv, parentNode, parentDom, isRoot);
+                    return newMenuNode;
+                }
+            }
+            let rootMenuNode = new MenuNode();
+            rootMenuNode.isRoot = true;     // Start with root nodes
+            
+            
             // Get container object
             if(typeof container == "string") {
                 container = document.querySelector(container);
@@ -21,48 +78,68 @@ SmartModule.addModule("menu", (
                 menuBar = container.querySelector(".sm-menu");
             }
             menuBar.classList.add("inactive");
-            buildMenu(menuElements, true);
+            let _rootMenuElement = buildMenu(menuElements, true);
             
             // Build the menu elements by traversing the menu tree
-            function buildMenu(menu, isRoot, parent) {
-                parent = parent || menuBar;
+            function buildMenu(menu, isRoot, parentDom, parentNode) {
+               // console.log(menu, isRoot, parentDom, parentNode);
+                parentNode = parentNode || rootMenuNode;
+                parentDom = parentDom || menuBar;
+
                 let menuGroupDiv = document.createElement("div");
                 menuGroupDiv.classList.add("sm-menu-group");
                 if(isRoot) menuGroupDiv.classList.add("sm-menu-root");     // Root menu items are styled differently than their sub menus.   
-                parent.append(menuGroupDiv);        
+                parentDom.append(menuGroupDiv);
                 menu.map(menuItem => {
-                    let menuDiv = null;
-                    if(typeof menuItem == "string") {
-                        menuDiv = document.createElement("hr");
-                        
-                    } else {
-                        menuDiv = document.createElement("div");
-                        menuDiv.classList.add("sm-menu-item");
-                        menuDiv.innerHTML = "<span>" + menuItem.title + "</span>";
-                        menuDiv.addEventListener("click", e=>{
-                            if( menuBar.classList.contains("inactive") ) {
-                                menuBar.classList.remove("inactive");
-                            } else {
-                                menuBar.classList.add("inactive");
-                            }
-                            e.stopPropagation();
-                            menuItem.action(e);
-                        });
-                    }
-                    menuGroupDiv.append(menuDiv);
-               
-                    if(menuItem.submenu) {
-                        let newMenu = buildMenu(menuItem.submenu, false, menuDiv);
-                        // menuDiv.addEventListener("click", e=>showSubMenu(newMenu, e.target));
-                        menuDiv.addEventListener("mouseenter", e=>{
-                            // do something
-                        });
-                        if(isRoot) { menuDiv.classList.add("sm-menu-rootitem"); } else { menuDiv.classList.add("sm-menu-haschildren"); }
-                        // menuDiv.addEventListener("mouseleave", ()=>hideSubMenu(newMenu));
- 
-                    };
+                    addMenuNode(menuItem, menuGroupDiv, parentNode, parentDom, isRoot);
                 });
                 return menuGroupDiv;
+            }
+
+            // Adds a menu item to a menu group element
+            function addMenuNode(menuItem, menuGroupDiv, parentNode, parentDom, isRoot) {
+                let menuNode = new MenuNode();
+                menuNode.parentNode = parentNode;                    
+                let menuDiv = null;
+                let divider = false;
+                if(typeof menuItem == "string") {
+                    menuDiv = document.createElement("hr"); // Horizontal line element
+                    menuNode.name = "__divider__";
+                    menuNode = parentNode;
+                    divider = true;
+                } else {
+                    menuDiv = document.createElement("div");
+                    menuDiv.classList.add("sm-menu-item");
+                    menuDiv.innerHTML = "<span>" + menuItem.title + "</span>";
+                    menuNode.name = menuItem.title;
+                    menuDiv.addEventListener("click", e=>{
+                        if( menuBar.classList.contains("inactive") ) {
+                            menuBar.classList.remove("inactive");
+                        } else {
+                            menuBar.classList.add("inactive");
+                        }
+                        e.stopPropagation();
+                        if(menuItem.action) menuItem.action(e);
+                    });
+                }
+                menuGroupDiv.append(menuDiv);
+                
+                menuNode.domElement = menuDiv;
+                menuNode.parentDom = parentDom;
+                menuNode.subMenus = [];
+                if(parentNode) parentNode.subMenus.push(menuNode);
+                if(menuItem.submenu) {
+                    let newMenu = buildMenu(menuItem.submenu, false, menuDiv, menuNode);
+                    // menuDiv.addEventListener("click", e=>showSubMenu(newMenu, e.target));
+                    menuDiv.addEventListener("mouseenter", e=>{
+                        // do something
+                    });
+                    if(isRoot) { menuDiv.classList.add("sm-menu-rootitem"); } else { menuDiv.classList.add("sm-menu-haschildren"); }
+                    // menuDiv.addEventListener("mouseleave", ()=>hideSubMenu(newMenu));
+
+                };
+                if(isRoot) { menuDiv.classList.add("sm-menu-rootitem"); }
+                if(divider) { return parentNode; } else { return menuNode; }
             }
 
             menuBar.addEventListener("click", ()=>{
@@ -72,8 +149,8 @@ SmartModule.addModule("menu", (
                     menuBar.classList.add("inactive");
                 }
             });
-
-            return menuBar;
+            rootMenuNode.domElement = _rootMenuElement;
+            return rootMenuNode;
         },
      }
 ));
